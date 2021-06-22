53c965cc5006a7ec64badd85cf928836e312d87f
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-22 13:49:40 -0400

    outlined api routes needed based on api.js fetch calls

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+const Workout = require('../models/workout.js')
+
+// GET route /api/workouts
+
+// PUT route /api/workouts/:id
+
+// POST route /api/workouts
+
+// GET route /api/workouts/range
+
