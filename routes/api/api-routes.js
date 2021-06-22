3bb50b7225bfaaa9a0205e2134388b27c2e25d26
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-22 14:16:34 -0400

    finished get route for api/workouts

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -2,10 +2,24 @@ const router = require('express').Router();
 const Workout = require('../models/workout.js')
 
 // GET route /api/workouts
+router.get("/api/workouts", async (req,res) => {
+    try{ const data = await Workout.aggregate([
+        {$addFields: {
+            totalDuration:{$sum :'$exercises.duration' }
+        }}
+    ])
+    res.json(data)
+    res.console('success!')
+} catch (err) {
+    res.status(400).json(err)
+};
 
 // PUT route /api/workouts/:id
 
+
 // POST route /api/workouts
 
+
 // GET route /api/workouts/range
+
 
