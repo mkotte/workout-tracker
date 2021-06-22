00bbfd6272b75aaacf88870ae2583012af04d957
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-22 14:22:55 -0400

    finished put route, framed final routes

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -15,11 +15,24 @@ router.get("/api/workouts", async (req,res) => {
 };
 
 // PUT route /api/workouts/:id
-
+router.put("/api/workouts/:id", async (req,res) => {
+    try{ const data = await Workout.findByIdAndUpdate(req.params.id,
+        {$push : {exercises: req.body} },
+        {new: true});
+        res.json(data)
+        res.console('success!')
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
 
 // POST route /api/workouts
+router.post("/api/workouts", async (req,res) => {
 
+});
 
 // GET route /api/workouts/range
+router.get("/api/workouts/range", async (req,res) => {
 
+});
 
